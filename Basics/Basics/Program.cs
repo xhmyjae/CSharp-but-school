7c0132b03711a9beaf9bae77e5b5ab9ba269fd01
xhmyjae <tomatis.mt@gmail.com>
2022-09-19 09:41:18 +0200

    feat : basics exercises start

---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+
+namespace Basics
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int intValue = 50;
+            Console.WriteLine(intValue);
+            string stringValue = "Bonjour";
+            Console.WriteLine(stringValue);
+            float floatValue = 3.1415F;
+            Console.WriteLine(floatValue);
+            char charValue = 'a';
+            Console.WriteLine(charValue);
+            bool boolValue = true;
+            Console.WriteLine(boolValue);
+            const int constValue = 10;
+            Console.WriteLine(constValue);
+            int x = 10;
+            int y = 50;
+            Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y);
+            string text1 = "blabla 1 ";
+            string text2 = "2 blabla";
+            Console.WriteLine(text1 + text2);
+            Console.WriteLine($"{text1}{text2}");
+            int num1, num2, num3;
+            char thirdValue = text1[3];
+            Console.WriteLine(thirdValue);
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+
+        }
+    }
+}
